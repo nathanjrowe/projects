15f17d137b6b451136f5541c53be6afb6fa59534
Nathan Rowe <naterowe2002@gmail.com>
2024-06-24 15:25:14 -0600

    more inheritance practice

--- js/js-inheritance.js
+++ js/js-inheritance.js
@@ -82,3 +82,70 @@ class Media {
   console.log(speed.isCheckedOut);
   speed.addRating(1); speed.addRating(1); speed.addRating(5);
   console.log(speed.getAverageRating());
+
+  class School {
+    constructor(name, level, numberOfStudents) {
+      this._name = name;
+      this._level = level;
+      this._numberOfStudents = numberOfStudents;
+    }
+  
+    get name() {
+      return this._name;
+    }
+    get level() {
+      return this._level;
+    }
+    get numberOfStudents() {
+      return this._numberOfStudents;
+    }
+    set numberOfStudents(num) {
+      if(typeof num === 'number' && num >= 0) {
+        this._numberOfStudents = num;
+      } else {
+        console.log('Invalid Input: numberOfStudents must be set to a number.');
+      }
+    }
+  
+    quickFacts() {
+      console.log(`${this._name} educates ${this._numberOfStudents} students at the ${this._level} school level.`);
+    }
+    static pickSubstituteTeacher(substituteTeachers) {
+      return substituteTeachers[Math.floor(Math.random()*(substituteTeachers.length - 1))];
+    }
+  }
+  
+  class Primary extends School {
+    constructor(name, numberOfStudents, pickupPolicy) {
+      super(name, 'primary', numberOfStudents);
+      this._pickupPolicy = pickupPolicy;
+    }
+  
+    get pickupPolicy() {
+      return this._pickupPolicy;
+    }
+  }
+  
+  class Middle extends School {
+    constructor(name, numberOfStudents) {
+      super(name, 'middle', numberOfStudents);
+    }
+  }
+  
+  class High extends School {
+    constructor(name, numberOfStudents, sportsTeams) {
+      super(name, 'high', numberOfStudents);
+      this._sportsTeams = sportsTeams;
+    }
+  
+    get sportsTeams() {
+      return this._sportsTeams;
+    }
+  }
+  
+  const lorraineHansbury = new Primary('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
+  lorraineHansbury.quickFacts();
+  School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']);
+  
+  const alSmith = new High('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field']);
+  console.log(alSmith.sportsTeams);
